22a36ce21c8a997565393f1c44c086c12ebba9eb
Milan Vodák <milanvodak02@gmail.com>
2024-04-15 01:45:08 +0200

    feat: implement EXIT instruction

--- student/Instruction/ExitInstruction.php
+++ student/Instruction/ExitInstruction.php
@@ -2,14 +2,30 @@
 
 namespace IPP\Student\Instruction;
 
-use IPP\Core\Exception\NotImplementedException;
+use IPP\Student\ArgType;
+use IPP\Student\DataType;
 use IPP\Student\Environment;
+use IPP\Student\Exception\OperandTypeError;
+use IPP\Student\Exception\OperandValueError;
 use IPP\Student\Instruction;
 
 class ExitInstruction extends Instruction
 {
+    protected array $expectedArgs = [ArgType::SYMB];
+
     public function execute(Environment $env): void
     {
-        throw new NotImplementedException;
+        $code = $env->resolve($this->args[0]);
+
+        $codeValue = $code->getValue();
+
+        if ($code->getType() !== DataType::INT) {
+            throw new OperandTypeError($this);
+        }
+        if ($codeValue < 0 || $codeValue > 9) {
+            throw new OperandValueError($this);
+        }
+
+        exit($codeValue);
     }
 }
