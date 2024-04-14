d70a1efac868d8d67ec2a13db22d74024c634eb8
Milan Vodák <milanvodak02@gmail.com>
2024-04-14 21:55:45 +0200

    feat: make semantic error message more detailed

--- student/Exception/SemanticError.php
+++ student/Exception/SemanticError.php
@@ -4,12 +4,17 @@ namespace IPP\Student\Exception;
 
 use IPP\Core\Exception\IPPException;
 use IPP\Core\ReturnCode;
+use IPP\Student\Instruction;
 use Throwable;
 
 class SemanticError extends IPPException
 {
-    public function __construct(string $message = "Semantic error", ?Throwable $previous = null)
+    public function __construct(?Instruction $instruction = null, string $message = "Semantic error", ?Throwable $previous = null)
     {
+        if ($instruction != null) {
+            $message = "Instruction " . $instruction->getOrder() . " (" . $instruction->getOpcode() . "): " . $message;
+        }
+
         parent::__construct($message, ReturnCode::SEMANTIC_ERROR, $previous, false);
     }
 }
