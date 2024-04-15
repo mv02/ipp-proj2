c83ab7a239ea2e002e7296b83159c5e6c59daf78
Milan Vodák <milanvodak02@gmail.com>
2024-04-15 17:50:52 +0200

    feat: implement READ instruction

--- student/Instruction/ReadInstruction.php
+++ student/Instruction/ReadInstruction.php
@@ -2,14 +2,26 @@
 
 namespace IPP\Student\Instruction;
 
-use IPP\Core\Exception\NotImplementedException;
+use IPP\Student\ArgType;
+use IPP\Student\DataType;
 use IPP\Student\Environment;
 use IPP\Student\Instruction;
 
 class ReadInstruction extends Instruction
 {
+    protected array $expectedArgs = [ArgType::VAR, ArgType::TYPE];
+
     public function execute(Environment $env): void
     {
-        throw new NotImplementedException;
+        $destArg = $this->args[0];
+
+        $type = DataType::from($this->args[1]->getValue());
+        $value = $env->read($type);
+
+        if ($value === null) {
+            $type = DataType::NIL;
+        }
+
+        $env->set($destArg->getName(), $destArg->getFrame(), $type, $value);
     }
 }
