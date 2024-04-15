50a85125639bfb0cedac997b4777987c792c5c44
Milan Vodák <milanvodak02@gmail.com>
2024-04-15 10:44:01 +0200

    feat: implement INT2CHAR/STRI2INT instructions

--- student/Instruction/Int2CharInstruction.php
+++ student/Instruction/Int2CharInstruction.php
@@ -2,14 +2,35 @@
 
 namespace IPP\Student\Instruction;
 
-use IPP\Core\Exception\NotImplementedException;
+use IPP\Student\ArgType;
+use IPP\Student\DataType;
 use IPP\Student\Environment;
+use IPP\Student\Exception\StringOperationError;
+use IPP\Student\Exception\OperandTypeError;
 use IPP\Student\Instruction;
 
 class Int2CharInstruction extends Instruction
 {
+    protected array $expectedArgs = [ArgType::VAR, ArgType::SYMB];
+
     public function execute(Environment $env): void
     {
-        throw new NotImplementedException;
+        $destArg = $this->args[0];
+
+        $ord = $env->resolve($this->args[1]);
+
+        $ordValue = $ord->getValue();
+
+        if ($ord->getType() !== DataType::INT) {
+            throw new OperandTypeError($this);
+        }
+
+        $char = mb_chr($ordValue);
+
+        if ($char === false) {
+            throw new StringOperationError($this, "Invalid ordinal value");
+        }
+
+        $env->set($destArg->getName(), $destArg->getFrame(), DataType::STRING, $char);
     }
 }
