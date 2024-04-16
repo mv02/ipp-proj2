442775ee17dc2766b176c9bb79b44cb8f6c91920
Milan Vodák <milanvodak02@gmail.com>
2024-04-16 22:32:35 +0200

    fix: disallow writing an uninitialized variable

--- student/Instruction/WriteInstruction.php
+++ student/Instruction/WriteInstruction.php
@@ -3,7 +3,9 @@
 namespace IPP\Student\Instruction;
 
 use IPP\Student\ArgType;
+use IPP\Student\DataType;
 use IPP\Student\Environment;
+use IPP\Student\Exception\ValueError;
 use IPP\Student\Instruction;
 
 class WriteInstruction extends Instruction
@@ -15,6 +17,10 @@ class WriteInstruction extends Instruction
         $arg = $this->args[0];
 
         $symbol = $env->resolve($arg);
+
+        if ($symbol->getType() === DataType::NONE) {
+            throw new ValueError();
+        }
 
         $env->write($symbol);
     }
